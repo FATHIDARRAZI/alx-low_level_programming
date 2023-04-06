82bd8141e1b7c857e7841a1627a17381065953ca
Abdelfattah Darrazi <abdelfattahdarrazi@gmail.com>
2023-04-05 19:54:56 -0700

    s

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,39 @@
 #include "main.h"
-
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-	int length;
-
-	length = _strlen_recursion(s);
-	if (length <= 1)
-		return (1);
-
-	return (check_palindrome(s, 0, length - 1));
-}
-
-/**
- * check_palindrome - checks if a substring of a string is a palindrome
- * @s: the string to check
- * @start: the index to start checking from
- * @end: the index to stop checking at
- *
- * Return: 1 if the substring is a palindrome, 0 otherwise
- */
-int check_palindrome(char *s, int start, int end)
-{
-	if (s[start] != s[end])
-		return (0);
-
-	if (start >= end)
-		return (1);
-
-	return (check_palindrome(s, start + 1, end - 1));
-}
-
-/**
- * _strlen_recursion - returns the length of a string
- * @s: the string to get the length of
- *
- * Return: the length of the string
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * pal_checker - check if s is palindrome.
+ * @s: string base address.
+ * @i: left index.
+ * @j: rigth index.
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int pal_checker(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
 		return (0);
-
-	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * is_palindrome - check if s is palindrome
+ * @s: base address for string.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered
- * identical, otherwise returns 0. The second string can contain a wildcard
- * character '*'.
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * @s1: first string
- * @s2: second string, may contain wildcard character '*'
- *
- * Return: 1 if strings can be considered identical, 0 otherwise.
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 == '\0' || *s2 == '*')
-			return (1);
-		else
-			return (0);
-	}
-	else if (*s2 == '*')
-	{
-		if (wildcmp(s1, s2 + 1) == 1 || wildcmp(s1 + 1, s2) == 1)
-			return (1);
-		else
-			return (0);
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	else
-	{
-		return (0);
-	}
+	return (str_checker(s1, s2, 0, 0));
 }
