01a3a5e5fe6e797e037e5336b1f1fc0db48a2125
Abdelfattah Darrazi <abdelfattahdarrazi@gmail.com>
2023-04-05 19:38:05 -0700

    s

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,17 +16,19 @@ int is_prime_number(int n)
 }
 
 /**
- * check_prime - checks if a number is prime
+ * check_prime - helper function to check if a number is prime
  * @n: the number to check
- * @i: the divisor
+ * @i: the divisor to try
  *
- * Return: 1 if prime, 0 otherwise
+ * Return: 1 if the number is prime, 0 otherwise
  */
 int check_prime(int n, int i)
 {
+    if (n < 2)
+        return (0);
     if (n % i == 0)
         return (0);
-    if (i > n / 2)
+    if (i * i > n)
         return (1);
     return (check_prime(n, i + 2));
 }
